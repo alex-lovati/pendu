7a179978f89b44447a8256007c0de277225ccf08
alex <alex.lovati@laplateforme.io>
2022-02-15 13:34:50 +0100

    transfert de travail '-'

--- admin.php
+++ admin.php
@@ -1,30 +1,37 @@
 <?php
 require_once ('FONCTION/fonction.php');
 
-if(!isset($listmot)){
-    $listmot = file("mots.txt");
-    $mottotal = count($listmot,) -1;
+if(!isset($arrayMot)){
+    $arrayMot = file("mots.txt");
+    $mottotal = count($arrayMot,) -1;
 }
 if(isset($_GET["lemot"])){
-    $key = $_GET["lemot"];
+
+    if(!isset($arrayMot)){
+    $msg = "le jeu doit contenir un mot";
+    header("refresh:5; url=admin.php");
+}else{
+    $key = strip_tags(htmlspecialchars($_GET["leMot"]));
     unset($arrayMot[$key]);
     file_put_contents("mot.txt", $arrayMot);
     header("location: admin.php");
 }
+}
 if (isset ($_POST ["nouveaumot"])){
-    $msg=false;
-    $Nouveau= $POST ["nouveau-mot"];
-    $nouveaumot = deleteSpecialChar(strtolower($Nouveau));
-
-    foreach ($arrayMot as $key=>$mot){
-        if($nouveaumot === $mot){
+    $Nouveau=strip_tags(htmlspecialchars($_POST["nouveaumot"]));
+    $nouveaumot= $Nouveau;
+    if(strlen($_POST["nouveaumot"])>=20){
+        $msg = "le mot doit comporter moi de 20 lettres";
+    }
+    foreach (chooseWord($arrayMot) as $key=>$mot){
+        if($mot == $nouveaumot){
             $msg = " mot non disponible";
         }
     }
-    if (!$msg){
+    if (!isset($msg)){
         $fichierMot = fopen('mot.txt','a+');
-        fputs ($fichierMot, $nouveaumot . "/n");
-        $msg = "mot rentrer dans le jeu";
+        fputs ($fichierMot, $nouveaumot ."/n");
+        $goodmsg = "mot rentrer dans le jeu";
     }
     header("location: admin.php");
 }
@@ -46,8 +53,8 @@ if (isset ($_POST ["nouveaumot"])){
                 <article>
                     <h1>Entre un nouveau mot !</h1>
                     <form action="" method="POST">
-                        <label for="newMot">Voulez vous ajouter un nouveau mot ? (<i>caractère spéciaux et les nombres sont interdit</i>)</label>
-                        <input type="text" id="newMot" name="newMot">
+                        <label for="nouveaumot">Met ton nouveau mot ici ^^ -></label>
+                        <input type="text" id="nouveaumot" name="nouveaumot">
                         <input class="btn btn-primary" type="submit" name="enoyer" value="envoyer">
                     </form>
                     <a class="btn btn-primary" href="index.php">Retourner jouer !</a>
